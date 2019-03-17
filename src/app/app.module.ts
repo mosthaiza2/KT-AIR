911c6acb1616105f939d9a726e38575831027d73
Krisana Tubsaithong <mosthsiza@gmail.com>
2019-03-17 18:07:00 +0700

    Update customerDetail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import {HttpClientModule} from '@angular/common/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -10,6 +11,8 @@ import {AboutPage} from '../pages/about/about';
 import {ContactPage} from '../pages/contact/contact';
 import {LocationPage} from '../pages/location/location';
 import {SlidesPage} from '../pages/slides/slides';
+import { CustomersProvider } from '../providers/customers/customers';
+import {CustomerdetailPage} from '../pages/customerdetail/customerdetail';
 @NgModule({
   declarations: [
     MyApp,
@@ -17,9 +20,11 @@ import {SlidesPage} from '../pages/slides/slides';
     AboutPage,
     ContactPage,
     LocationPage,
-    SlidesPage
+    SlidesPage,
+    CustomerdetailPage
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
@@ -30,12 +35,14 @@ import {SlidesPage} from '../pages/slides/slides';
     AboutPage,
     ContactPage,
     LocationPage,
-    SlidesPage
+    SlidesPage,
+    CustomerdetailPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    CustomersProvider
   ]
 })
 export class AppModule {}
